9ba9c7286fd8712e00a53e97eefc5ed1a3e66bdd
liorb <lior.buchmeier@gmail.com>
2023-01-03 13:14:17 +0200

    settings update

--- djangoweather/settings.py
+++ djangoweather/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!__u91xy7ij-^)zbcsyfyj6e$(68v1rhlqjixb!$#!@ty0dzt6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
